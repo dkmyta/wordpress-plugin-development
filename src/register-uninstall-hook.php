5272808c9a136e1b2dbae1917dcf8358edf83107
dkmyta <dkmyta10@gmail.com>
2021-07-05 14:15:51 -0700

    Commented out uninstall hook registration due to existence of uninstall.php alternative, kept for learning purposes

--- src/register-uninstall-hook.php
+++ src/register-uninstall-hook.php
@@ -1,5 +1,8 @@
 <?php
-
+/*
+* Alternate method for initiated uninstallation 
+* if uninstall.php did not exist
+*
 register_uninstall_hook( __FILE__, 'pdev_uninstall' );
 
 function pdev_uninstall() {
@@ -9,3 +12,4 @@ function pdev_uninstall() {
 		$role->remove_cap( 'pdev_manage' );
 	}
 }
+*/
