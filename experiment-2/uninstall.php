cd122ccc23329197c153e29efc4efd752b6a995b
dkmyta <dkmyta10@gmail.com>
2021-07-15 11:57:39 -0700

    Add uninstall hook to unregister settings and delete options

--- experiment-2/uninstall.php
+++ experiment-2/uninstall.php
@@ -13,3 +13,17 @@ $role = get_role( 'administrator' );
 if ( ! empty( $role ) ) {
 	$role->remove_cap( 'pdev_manage' );
 }
+
+// Register our uninstall function
+register_uninstall_hook( __FILE__, 'pdev_plugin_uninstall' );
+
+// Deregister our settings group and delete all options
+function pdev_plugin_uninstall() {
+
+	// Clean de-registration of registered setting
+	unregister_setting( 'pdev_plugin_options', 'pdev_plugin_options' );
+
+	// Remove saved options from the database
+	delete_option( 'pdev_plugin_options' );
+
+}
