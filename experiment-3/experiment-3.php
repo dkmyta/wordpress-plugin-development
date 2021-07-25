388ff9a8a261463a15cac7803f8905208b3997fa
dkmyta <dkmyta10@gmail.com>
2021-07-25 12:48:18 -0700

    Introduce Related Posts functionality in plugin using WP Cache API

--- experiment-3/experiment-3.php
+++ experiment-3/experiment-3.php
@@ -401,3 +401,60 @@ function pdev_nonce_example_template() { ?>
 		</form>
 	</div>
 <?php }
+
+// Apply Related Posts functionality using WordPress Cache API
+add_filter( 'the_content', 'pdev_related_posts' );
+
+function pdev_related_posts( $content ) {
+
+	// Bail if not viewing a single post.
+	if ( ! is_singular( 'post' ) || ! in_the_loop() ) {
+		return $content;
+	}
+
+	// Get the current post ID.
+	$post_id = get_the_ID();
+
+	// Check for cached posts.
+	$posts = wp_cache_get( $post_id, 'pdev_related_posts' );
+
+	// If no cached posts, query them.
+	if ( ! isset( $cache ) ) {
+		$categories = get_the_category();
+
+		$posts = get_posts( [
+			'category' => absint( $categories[0]->term_id ),
+			'post__not_in' => [ $post_id ],
+			'numberposts'  => 5
+		] );
+
+		// Save the cached posts.
+		if ( $posts ) {
+			wp_cache_set(
+				$post_id,
+				$posts,
+				'pdev_related_posts',
+				DAY_IN_SECONDS
+			);
+		}
+	}
+
+	// If posts were found at this point.
+	if ( $posts ) {
+
+		$content .= '<h3>Related Posts</h3>';
+
+		$content .= '<ul>';
+
+		foreach ( $posts as $post ) {
+			$content .= sprintf(
+				'<li><a href="%s">%s</a></li>',
+				esc_url( get_permalink( $post->ID ) ),
+				esc_html( get_the_title( $post->ID ) )
+			);
+		}
+
+		$content .= '</ul>';
+	}
+    return $content;
+}
