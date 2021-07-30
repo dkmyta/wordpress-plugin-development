e3d3582c5bcd8b64045029193d03b4318b9cec59
dkmyta <dkmyta10@gmail.com>
2021-07-30 11:52:19 -0700

    Remove uninstall hook registration, leaving only unregister settings and delete options functionality

--- experiment-4/uninstall.php
+++ experiment-4/uninstall.php
@@ -15,10 +15,10 @@ if ( ! empty( $role ) ) {
 }
 
 // Register our uninstall function
-register_uninstall_hook( __FILE__, 'pdev_plugin_uninstall' );
+//register_uninstall_hook( __FILE__, 'pdev_plugin_uninstall' );
 
 // Deregister our settings group and delete all options
-function pdev_plugin_uninstall() {
+//function pdev_plugin_uninstall() {
 
 	// Clean de-registration of registered setting
 	unregister_setting( 'pdev_plugin_options', 'pdev_plugin_options' );
@@ -26,4 +26,4 @@ function pdev_plugin_uninstall() {
 	// Remove saved options from the database
 	delete_option( 'pdev_plugin_options' );
 
-}
+//}
