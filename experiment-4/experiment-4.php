4e9f0607b9547fc951276cc5461e6ec34ff07734
dkmyta <dkmyta10@gmail.com>
2021-08-01 12:34:45 -0700

    Introduce add_filter example using template_include hook to apply a template when a certain custom post type is used

--- experiment-4/experiment-4.php
+++ experiment-4/experiment-4.php
@@ -622,3 +622,30 @@ function pdev_content_subscription_form( $content ) {
 
 	return $content;
 }
+
+// add_filter examples using template_include hook to use specific template when a certain custom post type is used
+add_filter( 'template_include', 'pdev_template_include' );
+
+function pdev_template_include( $template ) {
+
+	if ( is_post_type_archive( 'movie' ) ) {
+
+		$template = locate_template( 'pdev-movie-archive.php' );
+
+		if ( ! $locate ) {
+			$template = require_once plugin_dir_path( __FILE__ )
+			            . 'templates/pdev-movie-archive.php';
+		}
+
+	} elseif ( is_singular( 'movie' ) ) {
+
+		$template = locate_template( 'pdev-single-movie.php' );
+
+		if ( ! $locate ) {
+			$template = require_once plugin_dir_path( __FILE__ )
+			            . 'templates/pdev-single-movie.php';
+		}
+	}
+
+	return $template;
+}
