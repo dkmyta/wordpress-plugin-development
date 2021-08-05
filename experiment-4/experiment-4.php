9640c3b414162e5dc3628c0f61c1b816b353700f
dkmyta <dkmyta10@gmail.com>
2021-08-05 12:07:08 -0700

    Introduce add_action example to create a custom widget using the widgits_init hook

--- experiment-4/experiment-4.php
+++ experiment-4/experiment-4.php
@@ -652,3 +652,77 @@ function pdev_template_include( $template ) {
 
 	return $template;
 }
+
+// add_actions examples using widgits_init hook to create the widget 
+class deans_widget extends WP_Widget {
+  
+	function __construct() {
+	parent::__construct(
+	
+	// Base ID of your widget
+	'deans_widget', 
+	
+	// Widget name will appear in UI
+	__('Dean\'s Widget', 'deans_widget_domain'), 
+	
+	// Widget description
+	array( 'description' => __( 'Sample widget', 'deans_widget_domain' ), ) 
+	);
+
+	}
+	
+	// Creating widget front-end
+	
+	public function widget( $args, $instance ) {
+
+		$title = apply_filters( 'widget_title', $instance['title'] );
+	
+		// before and after widget arguments are defined by themes
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+		echo $args['before_title'] . $title . $args['after_title'];
+		
+		// This is where you run the code and display the output
+		echo __( 'Hello, World!', 'deans_widget_domain' );
+		echo $args['after_widget'];
+
+	}
+			
+	// Widget Backend 
+	public function form( $instance ) {
+
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		} else {
+			$title = __( 'New title', 'deans_widget_domain' );
+		}
+	// Widget admin form
+	?>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+	</p>
+	<?php 
+
+	}
+		
+	// Updating widget replacing old instances with new
+	public function update( $new_instance, $old_instance ) {
+
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+
+	}
+	
+	// Class wpb_widget ends here
+} 
+ 
+// Register and load the widget
+function deans_load_widget() {
+
+    register_widget( 'deans_widget' );
+
+}
+
+add_action( 'widgets_init', 'deans_load_widget' );
