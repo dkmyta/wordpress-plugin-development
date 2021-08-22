d6c55f374c89b797f3fe1453a4efb2196185056a
dkmyta <dkmyta10@gmail.com>
2021-08-22 08:47:12 -0700

    Apply corrections to original block creation files

--- experiment-5/webpack.config.js
+++ experiment-5/webpack.config.js
@@ -12,13 +12,16 @@ module.exports = {
     module: {
         rules: [
             {
-                test:   /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: [
-						'babel-preset-env',
-						'babel-preset-react'
-					]
+                test:   /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: { 
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+						'@babel/preset-env',
+						'@babel/preset-react'
+					    ]   
+                    }                 
                 }
             }
         ]
