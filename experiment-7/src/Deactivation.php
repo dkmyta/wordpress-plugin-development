10abd184b6342570625a78c36b6eaf4227b4be56
dkmyta <dkmyta10@gmail.com>
2021-09-14 11:51:36 -0700

    Apply creation/removal of custom forums roles/capabilities on activation/deactivation

--- experiment-7/src/Deactivation.php
+++ experiment-7/src/Deactivation.php
@@ -6,5 +6,11 @@ class Deactivation {
 
 	public static function deactivate() {
 		// Run your deactivation code here.
+
+		// Remove custom forum roles.
+		remove_role( 'forum_administrator' );
+		remove_role( 'forum_moderator'     );
+		remove_role( 'forum_member'        );
+		remove_role( 'forum_banned'        );
 	}
 }
