ae7a1429e6717296d184224d52e9fc87102f2827
dkmyta <dkmyta10@gmail.com>
2021-09-16 11:54:09 -0700

    Apply both scheduled task practical use case examples - automated post revision removal and admin tool for comment removal

--- experiment-8/experiment-8.php
+++ experiment-8/experiment-8.php
@@ -733,3 +733,157 @@ function pdev_pester_check() {
 		wp_mail( $email, $subject, $message );
 	}
 }
+
+// Introduce a clean revisions tool using recurring cron events
+register_activation_hook( __FILE__, 'pdev_clean_rev_activate' );
+
+function pdev_clean_rev_activate() {
+
+	if ( ! wp_next_scheduled( 'pdev_clean_rev_event' ) ) {
+		wp_schedule_event( time(), 'weekly', 'pdev_clean_rev_event' );
+	}
+}
+
+register_deactivation_hook( __FILE__, 'pdev_clean_rev_deactivate' );
+
+function pdev_clean_rev_deactivate() {
+
+	$timestamp = wp_next_scheduled( 'pdev_clean_rev_event' );
+
+	if ( false !== $timestamp ) {
+		wp_unschedule_event( $timestamp, 'pdev_clean_rev_event' );
+	}
+}
+
+add_filter( 'cron_schedules', 'pdev_clean_rev_cron_schedules' );
+
+function pdev_clean_rev_cron_schedules( $schedules ) {
+
+	$schedules['weekly'] = [
+		'interval' => 604800,
+		'display'  => 'Once Weekly'
+	];
+
+	return $schedules;
+}
+
+add_action( 'pdev_clean_rev_event', 'pdev_clean_rev_delete' );
+
+function pdev_clean_rev_delete() {
+	global $wpdb;
+
+	$sql = "DELETE a,b,c
+	        FROM $wpdb->posts array
+		LEFT JOIN $wpdb->term_relationships b ON (a.ID = b.object_id)
+		LEFT JOIN $wpdb->postmeta c ON (a.ID = c.post_id)
+		WHERE a.post_type = 'revision'
+		AND DATEDIFF( now(), a.post_modified ) > 30";
+
+	$wpdb->query( $wpdb->prepare( $sql ) );
+}
+
+// Introduce a delete comments tools using recurring cron events
+function pdev_delete_comments_options() {
+
+	return get_option( 'pdev_delete_comments', [
+		'status' => 'spam',
+		'days'   => 15
+	] );
+}
+
+add_action( 'admin_init', 'pdev_delete_comments_init' );
+
+function pdev_delete_comments_init() {
+
+	// Register settings on the discussion screen.
+	register_setting(
+		'discussion',
+		'pdev_delete_comments'
+	);
+
+	// Register comment status field.
+	add_settings_field(
+		'pdev_comment_status',
+		'Comment Status to Delete',
+		'pdev_comment_status_field',
+		'discussion',
+		'default'
+	);
+
+	// Register days field.
+	add_settings_field(
+		'pdev_comment_days',
+		'Delete Comments Older Than',
+		'pdev_comment_days_field',
+		'discussion',
+		'default'
+	);
+
+	// Schedule the cron event if not scheduled.
+	if ( ! wp_next_scheduled( 'pdev_delete_comments_event' ) ) {
+		wp_schedule_event( time(), 'daily', 'pdev_delete_comments_event' );
+	}
+}
+
+function pdev_comment_status_field() {
+
+	$options = pdev_delete_comments_options();
+	$status  = $options['status']; ?>
+
+	<select name="pdev_delete_comments[status]">
+		<option value="spam" <?php selected( $status, 'spam' ); ?>>
+			Spam
+		</option>
+		<option value="moderated" <?php selected( $status, 'moderated' ); ?>>
+			Moderated
+		</option>
+	</select>
+
+<?php }
+
+function pdev_comment_days_field() {
+
+	$options = pdev_delete_comments_options();
+	$days    = absint( $options['days'] );
+
+	printf(
+		'<input type="number" name="pdev_delete_comments[days]" value="%s">',
+		esc_attr( $days )
+	);
+}
+
+add_action( 'pdev_delete_comments_event', 'pdev_delete_comments_task' );
+
+function pdev_delete_comments_task() {
+	global $wpdb;
+
+	$options = pdev_delete_comments_options();
+	$status  = $options['status'];
+	$days    = absint( $options['days'] );
+
+	// Set default comment_approved value to spam.
+	$comment_approved = 'spam';
+
+	// If moderated status, WordPress stores this as '0'.
+	if ( 'moderated' !== $status ) {
+		$comment_approved = '0';
+	}
+
+	// Build and run the query to delete comments.
+	$sql = "DELETE FROM $wpdb->comments
+	        WHERE ( comment_approved = '$comment_approved' )
+		AND DATEDIFF( now(), comment_date ) > %d";
+
+	$wpdb->query( $wpdb->prepare( $sql, $days ) );
+}
+
+register_deactivation_hook( __FILE__, 'pdev_delete_comments_deactivate' );
+
+function pdev_delete_comments_deactivate() {
+
+	$timestamp = wp_next_scheduled( 'pdev_delete_comments_event' );
+
+	if ( false !== $timestamp ) {
+		wp_unschedule_event( $timestamp, 'pdev_delete_comments_event' );
+	}
+}
