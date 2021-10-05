a44707aeb4fbc7ad9511cfb2f01bbee26a16ffa9
dkmyta <dkmyta10@gmail.com>
2021-10-05 10:39:33 -0700

    Apply example using the HTTP API to parse JSON from a remote horror movie API from sampleapis.com

--- experiment-10/experiment-10.php
+++ experiment-10/experiment-10.php
@@ -707,3 +707,55 @@ function pdev_custom_endpoint() {
     );
 
 }
+
+// Apply example using the HTTP API to parse JSON from a remote horror movie API
+// Register a custom page for your plugin
+add_action( 'admin_menu', 'pdev_create_menu' );
+             
+function pdev_create_menu() {
+             
+    // Create custom top-level menu
+    add_menu_page( 'PDEV Movies Page', 'PDEV Movies',
+        'manage_options', 'pdev-movies', 'pdev_movie_api_results',
+        'dashicons-smiley', 99 );
+             
+}
+
+// Request and display Movie API data
+function pdev_movie_api_results() {
+
+    // Set your API URL
+    $request = wp_remote_get( 'https://api.sampleapis.com/movies/horror' );
+
+    // If an error is returned, return false to end the request
+    if( is_wp_error( $request ) ) {
+        return false;
+    }
+
+    // Retrieve only the body from the raw response
+    $body = wp_remote_retrieve_body( $request );
+
+    // Decode the JSON string
+    $data = json_decode( $body );
+
+    // Verify the $data variable is not empty
+    if( ! empty( $data ) ) {
+        
+        echo '<ul>';
+
+        // Loop through the returned dataset 
+        foreach( $data as $movies ) {
+
+            echo '<li>';
+                echo '<a href="https://www.imdb.com/title/' . esc_attr( $movies->imdbId ) . '">';
+                echo '<img src="' . esc_url( $movies->posterURL ) . '" height="75"><br />';
+                echo esc_html( $movies->title );
+                echo '</a>';
+            echo '</li>';
+
+        }
+
+        echo '</ul>';
+    }
+
+}
