c59e7a319b6a8d81119126968190e73df64df4d9
dkmyta <dkmyta10@gmail.com>
2021-10-15 08:39:35 -0700

    Apply advanced dashboard widget example

--- experiment-12/experiment-12.php
+++ experiment-12/experiment-12.php
@@ -944,3 +944,76 @@ function pdev_simple_dashboard_register() {
 function pdev_simple_dashboard_display() {
 	echo '<p>Please contact support@example.com to report bugs.</p>';
 }
+
+// Apply advanced dashboard widget
+add_action( 'wp_dashboard_setup', 'pdev_advanced_dashboard_register' );
+
+// Registers dashboard widgets.
+function pdev_advanced_dashboard_register() {
+
+	wp_add_dashboard_widget(
+		'pdev-advanced-dashboard',
+		'Dean\'s Custom Feed',
+		'pdev_advanced_dashboard_display',
+		'pdev_advanced_dashboard_control'
+	);
+}
+
+// Dashboard widget callback.
+function pdev_advanced_dashboard_display() {
+
+	// Get widget option.
+	$feed_url = get_option( 'pdev_dashboard_feed_url' );
+
+	// If no feed, set to the WordPress.org news feed.
+	if ( ! $feed_url ) {
+		$feed_url = 'https://wordpress.org/news/feed';
+	}
+
+	// Open HTML wrapper.
+	echo '<div class="rss-widget">';
+
+	// Output RSS feed list.
+	wp_widget_rss_output(
+		esc_url_raw( $feed_url ),
+		[
+			'title'        => 'RSS Feed News',
+			'items'        => 5,
+			'show_summary' => true,
+			'show_author'  => false,
+			'show_date'    => true
+		]
+	);
+
+	// Close HTML wrapper.
+	echo '</div>';
+}
+
+// Dashboard widget control callback.
+function pdev_advanced_dashboard_control() {
+
+	// Check if option is set before saving.
+	if ( isset( $_POST['pdev_dashboard_feed_url'] ) ) {
+
+		// Update database option.
+		update_option(
+			'pdev_dashboard_feed_url',
+			esc_url_raw( $_POST['pdev_dashboard_feed_url'] )
+		);
+	}
+
+	// Get widget option.
+	$feed_url = get_option( 'pdev_dashboard_feed_url' ); ?>
+
+	<p>
+		<label>
+			RSS Feed URL:
+			<input
+				type="text"
+				name="pdev_dashboard_feed_url"
+				class="widefat"
+				value="<?php echo esc_url( $feed_url ); ?>"
+			/>
+		</label>
+	</p>
+<?php }
